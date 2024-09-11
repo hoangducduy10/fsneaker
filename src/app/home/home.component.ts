a663c2ecb71d93d62acd7e061e70d5c94040423e
DucDuy <duybung2004@gmail.com>
2024-09-11 22:18:20 +0700

    Create header and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
