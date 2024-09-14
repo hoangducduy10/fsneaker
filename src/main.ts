c2ed81d331ba6c10ff5ce439ac4469139654ffb8
DucDuy <duybung2004@gmail.com>
2024-09-14 17:54:24 +0700

    Update project with backend changes

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { HomeComponent } from './app/home/home.component';
-import { DetailProductComponent } from './app/detail-product/detail-product.component';
-import { OrderComponent } from './app/order/order.component';
-import { OrderConfirmComponent } from './app/order-confirm/order-confirm.component';
-import { LoginComponent } from './app/login/login.component';
-import { RegisterComponent } from './app/register/register.component';
-
-bootstrapApplication(DetailProductComponent).catch((err) => console.error(err));
